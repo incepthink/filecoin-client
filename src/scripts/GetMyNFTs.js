45f985f4421981b08e7b7f072641f1447837f5b7
incepthink <incepthink@gmail.com>
2026-02-10 20:36:39 +0530

    chnage

--- src/scripts/GetMyNFTs.js
+++ src/scripts/GetMyNFTs.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const getMyNFTs = async (collectionId) => {
   try {
     const id = collectionId || process.env.COLLECTION_ID;
+    const user = Cookies.get("user") ? JSON.parse(Cookies.get("user")) : null;
+    const userId = user?.id;
     const { data } = await axios.get(
-      `https://api.hashcase.co/platform/nfts/by-collection?collection_id=${id}`
+      `https://api.hashcase.co/platform/nfts/by-collection?collection_id=${id}&user_id=${userId}`
     );
     return data.nfts || [];
   } catch (error) {
